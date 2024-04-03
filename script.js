c5414b155ad5e08c75a6b26f7e1afbc771404a8b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-03 16:14:04 +0500

    Adding files

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-var p1Display= document.querySelector('#p1Display')
-var p2Display= document.querySelector('#p2Display')
+const p1Display= document.querySelector('#p1Display')
+const p2Display= document.querySelector('#p2Display')
 
-var p1Button= document.querySelector('#p1Button')
-var p2Button= document.querySelector('#p2Button')
+const p1Button= document.querySelector('#p1Button')
+const p2Button= document.querySelector('#p2Button')
 
 
 let p1Score =0;
 let p2Score =0;
 let winning_score= 3;
-let winning_score1= 3;
 let isGameOver= false;
 
 p1Button.addEventListener('click',function(){
     if(!isGameOver){
         p1Score +=1;
-    }
     if(p1Score === winning_score){
       isGameOver= true
     }
     p1Display.textContent= p1Score;
-    
+}
 })
+
 p2Button.addEventListener('click',function(){
     if(!isGameOver){
         p2Score +=1;
-    }
-    if(p2Score === winning_score1){
+    if(p2Score === winning_score){
       isGameOver= true
     }
-    p2Display.textContent= p2Score;
-    
+    p2Display.textContent= p1Score;
+}
 })
 
 var Reset = document.querySelector('#Reset');
